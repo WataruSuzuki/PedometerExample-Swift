6605e2a1b350aa6883bc9061d7d84ce5584e93cb
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-05-14 22:10:00 +0900

    Created xcode project

---
+++ PedometerExample/PedometerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerExampleApp.swift
+//  PedometerExample
+//
+//  Created by WataruSuzuki on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct PedometerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
