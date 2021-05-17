e191bd4daad00096ae858a37dcd51a45d10f9c9c
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-05-17 22:10:00 +0900

    Added new Views

---
+++ PedometerExample/MonitoringView.swift
@@ -0,0 +1,20 @@
+//
+//  MonitoringView.swift
+//  PedometerExample
+//
+//  Created by watsuzuk on 2021/05/28.
+//
+
+import SwiftUI
+
+struct MonitoringView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MonitoringView_Previews: PreviewProvider {
+    static var previews: some View {
+        MonitoringView()
+    }
+}
